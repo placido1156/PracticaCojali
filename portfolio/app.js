dfd737e6c6370c9a0832458f254f56ee79fa8156
Plácido Chinchilla <241709351+placido1156@users.noreply.github.com>
2026-05-13 00:09:13 +0200

    Añadidos JavaScript y lo aprendido en la sesión 5.

--- portfolio/app.js
+++ portfolio/app.js
@@ -4,7 +4,9 @@ if (localStorage.getItem("tema") === "oscuro") {
     document.body.classList.add("oscuro");
 }
 
-botonTema.addEventListener("click", function () {
+botonTema.addEventListener("click", cambiarTema);
+
+function cambiarTema() {
 
     document.body.classList.toggle("oscuro");
 
@@ -14,4 +16,73 @@ botonTema.addEventListener("click", function () {
         localStorage.setItem("tema", "claro");
     }
 
-});
+}
+
+const estudios = [
+
+    {
+        nombre: "Primaria y ESO",
+        centro: "Colegio Virgen de la Cabeza"
+    },
+
+    {
+        nombre: "Bachillerato de Ciencias y Tecnología",
+        centro: "IES Bernardo Balbuena"
+    },
+
+    {
+        nombre: "1º DAW (en proceso)",
+        centro: "IES Gregorio Prieto"
+    }
+
+];
+
+const formulario = document.getElementById("formulario-estudio");
+
+const listaEstudios = document.getElementById("lista-estudios");
+
+mostrarEstudios();
+
+formulario.addEventListener("submit", agregarEstudio);
+
+function mostrarEstudios() {
+
+    listaEstudios.innerHTML = "";
+
+    for (let estudio of estudios) {
+
+        const div = document.createElement("div");
+
+        div.classList.add("estudio");
+
+        div.innerHTML = `
+            <h3>${estudio.nombre}</h3>
+            <p>${estudio.centro}</p>
+        `;
+
+        listaEstudios.appendChild(div);
+
+    }
+
+}
+
+function agregarEstudio(evento) {
+
+    evento.preventDefault();
+
+    const nombre = document.getElementById("nombre-estudio").value;
+
+    const centro = document.getElementById("centro-estudio").value;
+
+    const nuevoEstudio = {
+        nombre: nombre,
+        centro: centro
+    };
+
+    estudios.push(nuevoEstudio);
+
+    mostrarEstudios();
+
+    formulario.reset();
+
+}
